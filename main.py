0de0d3436a2063c5a39b466e527bbac6195a21e4
Roman Spodaryk <roman.spodaryk@ukr.net>
2020-05-04 21:57:20 +0300

    added decoding and check if message is broken

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ def int_to_bit(x):
     return arr
 
 
-def check_if_even():
-    number_of_repeat = int(math.sqrt(len(int_to_bit(number))))
-    list_of_bites = int_to_bit(number)
+def bin_to_int(list_bits):
+    x = 0
+    for i in range(len(list_bits)):
+        x += list_bits[i] * 2 ** (len(list_bits) - i - 1)
+    return x
+
+
+def check_if_even(num):
+    number_of_repeat = int(math.sqrt(len(int_to_bit(num))))
+    list_of_bites = int_to_bit(num)
     for i in range(number_of_repeat):
         size_of_returns_list = int(len(list_of_bites) / 2)
         list_of_bites = divide_and_check([], list_of_bites, size_of_returns_list, True)
-    return list_of_bites
+    return list_of_bites[0]
 
 
 def divide_and_check(returns_list, list_bit, size_of_returns_list, is_stop):
@@ -38,9 +45,27 @@ def check(x, y):
     return 0 if x == y else 1
 
 
+def get_decode_message(list_of_bits):
+    message = []
+    for i in range(int(len(list_of_bits) - 1)):
+        message.append(list_of_bit[i])
+    return message
+
+
+def is_message_broken(message):
+    if message[-1] == check_if_even(bin_to_int(get_decode_message(message))):
+        return False
+    else:
+
+        return True
+
+
 number = 732
-list_of_bits = int_to_bit(number)
-print("list_of_data before adding even bit == " + str(list_of_bits))
-print("even bit == " + str(check_if_even()[0]))
-list_of_bits.append(check_if_even()[0])
-print("final_list == " + str(list_of_bits))
+list_of_bit = int_to_bit(number)
+print("number == " + str(number))
+print("list_of_data before adding even bit == " + str(list_of_bit))
+print("even bit == " + str(check_if_even(number)))
+list_of_bit.append(check_if_even(number))
+print("final_list == " + str(list_of_bit))
+print("decode message == " + str(get_decode_message(list_of_bit)))
+print("is message broken == " + str(is_message_broken(list_of_bit)))
